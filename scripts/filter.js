e8bfe2e29c1a73fded753851ea6dfe63945aca41
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-03-07 02:22:20 +0200

    added filters

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,47 +1,48 @@
-filterSelection("all")
+filterSelection('all');
+
 function filterSelection(c) {
-  let x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+  let x = document.getElementsByClassName('card');
+  if (c == 'all') { c = ''; }
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  for (let i = 0; i < x.length; i++) {
+    removeClass(x[i], 'show');
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
   }
+  
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
+function addClass(element, name) {
+  let arr1 = element.className.split(' ');
+  let arr2 = name.split(' ');
+  for (let i = 0; i < arr2.length; i++) {
     if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+      element.className += ' ' + arr2[i];
     }
   }
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
+function removeClass(element, name) {
+  let arr1 = element.className.split(' ');
+  let arr2 = name.split(' ');
+  for (let i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
-  element.className = arr1.join(" ");
+  element.className = arr1.join(' ');
 }
 
 // Add active class to the current control button (highlight it)
-let btnContainer = document.getElementById("myBtnContainer");
-let btns = btnContainer.getElementsByClassName("btn");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+let filterList = document.getElementById('filter-list');
+let filterItems = filterList.getElementsByClassName('filter');
+for (let i = 0; i < filterItems.length; i++) {
+  filterItems[i].addEventListener('click', function () {
+    let current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
   });
 }
+
+
